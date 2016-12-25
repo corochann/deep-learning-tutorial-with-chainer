048fb6a43441fa8a921e76883065c0f7e858f67d
corochann <acc1ssnn9terias@gmail.com>
2016-12-25 22:29:43 +0900

    refactoring

--- src/mnist/train_mnist_1.py
+++ src/mnist/train_mnist_1.py
@@ -13,22 +13,7 @@ from chainer import cuda
 from chainer import computational_graph
 from chainer import serializers
 
-
-# Network definition
-class MLP(chainer.Chain):
-
-    def __init__(self, n_units, n_out):
-        super(MLP, self).__init__(
-            # the size of the inputs to each layer will be inferred
-            l1=L.Linear(None, n_units),  # n_in -> n_units
-            l2=L.Linear(None, n_units),  # n_units -> n_units
-            l3=L.Linear(None, n_out),  # n_units -> n_out
-        )
-
-    def __call__(self, x):
-        h1 = F.relu(self.l1(x))
-        h2 = F.relu(self.l2(h1))
-        return self.l3(h2)
+import src.mnist.mlp as mlp
 
 
 def main():
@@ -56,7 +41,7 @@ def main():
     # Set up a neural network to train
     # Classifier reports softmax cross entropy loss and accuracy at every
     # iteration, which will be used by the PrintReport extension below.
-    model = L.Classifier(MLP(args.unit, 10))
+    model = L.Classifier(mlp.MLP(args.unit, 10))
     if args.gpu >= 0:
         chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
         model.to_gpu()  # Copy the model to the GPU
